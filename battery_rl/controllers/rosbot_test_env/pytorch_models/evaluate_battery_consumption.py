bf6f33910bd0c9f1301bd435ed7db4ec367b5571
CheongWoong <cw.kang@kaist.ac.kr>
2024-10-10 11:48:21 +0900

    update: real2sim2real transfer

--- battery_rl/controllers/rosbot_test_env/pytorch_models/evaluate_battery_consumption.py
+++ battery_rl/controllers/rosbot_test_env/pytorch_models/evaluate_battery_consumption.py
@@ -6,20 +6,29 @@ with open('vanilla.log', 'r') as fin:
 with open('battery.log', 'r') as fin:
     result_battery = fin.readlines()
 
+with open('real2sim.log', 'r') as fin:
+    result_real2sim = fin.readlines()
+
 len_episode_vanilla = eval(result_vanilla[0].replace('episode length:', '').strip())
 battery_penalty_vanilla = eval(result_vanilla[1].replace('battery penalty:', '').strip())
 len_episode_battery = eval(result_battery[0].replace('episode length:', '').strip())
 battery_penalty_battery = eval(result_battery[1].replace('battery penalty:', '').strip())
+len_episode_real2sim = eval(result_real2sim[0].replace('episode length:', '').strip())
+battery_penalty_real2sim = eval(result_real2sim[1].replace('battery penalty:', '').strip())
 
 print('='*20)
 print(f'Mean episode length')
 print(f'Baseline: {np.mean(len_episode_vanilla)}')
-print(f'Ours: {np.mean(len_episode_battery)}')
+print(f'Battery: {np.mean(len_episode_battery)}')
 print(f'{np.round((np.mean(len_episode_vanilla) - np.mean(len_episode_battery)) / np.mean(len_episode_vanilla)*100, 2)}% reduced (improvement)')
+print(f'Real2sim: {np.mean(len_episode_real2sim)}')
+print(f'{np.round((np.mean(len_episode_vanilla) - np.mean(len_episode_real2sim)) / np.mean(len_episode_vanilla)*100, 2)}% reduced (improvement)')
 print()
 
 print(f'Mean battery consumption')
 print(f'Baseline: {np.mean(battery_penalty_vanilla)}')
-print(f'Ours: {np.mean(battery_penalty_battery)}')
+print(f'Battery: {np.mean(battery_penalty_battery)}')
 print(f'{np.round((np.mean(battery_penalty_vanilla) - np.mean(battery_penalty_battery)) / np.mean(battery_penalty_vanilla)*100, 2)}% reduced (improvement)')
+print(f'Real2sim: {np.mean(battery_penalty_real2sim)}')
+print(f'{np.round((np.mean(battery_penalty_vanilla) - np.mean(battery_penalty_real2sim)) / np.mean(battery_penalty_vanilla)*100, 2)}% reduced (improvement)')
 print('='*20)
